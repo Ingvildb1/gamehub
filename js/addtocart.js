e2b464f8bacf72f09397d6e227e0a75dbf9297d0
Ingvildb <IngBer61224@stud.noroff.no>
2022-10-04 22:36:40 +0200

    addtocart update

--- js/addtocart.js
+++ js/addtocart.js
@@ -1,5 +1,5 @@
 // Cart
-let cartIcon = document.querySelector('.fa-shopping-bag');
+let cartIcon = document.querySelector('.addtocart');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector(".close-cart");
 
@@ -38,7 +38,7 @@ function ready(){
  }
 
  // Add to Cart
- var addCart = document.getElementsByClassName('addtocart')
+ var addCart = document.getElementsByClassName('addproduct-tocart')
  for (var i = 0; i < addCart.length; i++) {
     var button = addCart[i]
     button.addEventListener("click", addCartClicked);
@@ -91,18 +91,20 @@ function addProductToCart(title, price, productImg) {
     var cartItems = document.getElementsByClassName('cart-content')[0];
     var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
     for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title){
         alert("You have already add this item to cart");
         return;
     }
 }
+
 var cartBoxContent = `
-                     <img src="${productImg}" alt="" class="cart-img">
+                    <img src="${productImg}" alt="" class="cart-img">
                      <div class="detail-box">
                        <div class="cart-product-title">${title}</div>
                        <div class="cart-price">${price}</div>
                       <input type="number" value="1" class="cart-quantity">
                      </div>
-                     <!-- Remove Cart-->
+                  
                      <i class="fa-regular fa-trash cart-remove"></i>
 `;
 
@@ -111,6 +113,7 @@ cartItems.append(cartShopBox);
 cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener('click', removeCartItem);
 cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener('change', quantityChanged);
 
+}
 
 // Update Total
 function updateTotal(){
